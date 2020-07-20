9864e9e2e33b81be691802d0937b1f02eaff9ee1
Baranov Nikita <n.baranov@pixelplus.ru>
2020-07-20 14:36:51 +0300

    Добавил возможность называть свою задачу

--- src/component/item-add-form/index.js
+++ src/component/item-add-form/index.js
@@ -3,12 +3,38 @@ import React from 'react';
 import "./item-add-form.css"
 
 export default class ItemAddForm extends React.Component{
+
+    constructor(){
+        super();
+
+        this.state = {
+            label: ''
+        };
+
+
+        this.onLabelChange = (event) =>{
+            this.setState({
+                label: event.target.value
+            });
+        };
+
+        this.onSubmit = (event) => {
+            event.preventDefault();
+            this.props.onItemAdded(this.state.label)
+        };
+    }
+    
     render(){
         return (
-            <div className="item-add-form">
+            <form   className="item-add-form"
+                    onSubmit={this.onSubmit}>
+                <input  type="text"
+                        className="form-control"
+                        onChange={this.onLabelChange}
+                        placeholder="What needs to be done" />
                 <button className="item-add-form__btn"
-                onClick={() => this.props.onItemAdded("Hello World")}>Add item</button>
-            </div>
+                type="submit">Add item</button>
+            </form>
         )
     };
 };
